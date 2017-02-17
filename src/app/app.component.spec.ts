d8929757f6751993e85414a896e8694bcb4f494b
uryyyyyyy <koki305@gmail.com>
2017-02-18 08:14:18 +0900

    component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,57 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {BehaviorSubject} from 'rxjs/Rx';
+import {CounterService} from './services/counter.service';
 
-describe('AppComponent', () => {
-  beforeEach(() => {
+describe('HogeComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  const counterServiceMock = {point: new BehaviorSubject(0)};
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-    TestBed.compileComponents();
+      declarations: [AppComponent],
+      providers: [
+        {provide: CounterService, useValue: counterServiceMock}
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.point).toEqual(0);
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelectorAll('p')[1].textContent).toContain(`Point: 0`);
   }));
+
+  it('should call increment', () => {
+    spyOn(component, 'increment');
+    const buttons: NodeListOf<HTMLButtonElement> = fixture.debugElement.nativeElement.querySelectorAll('button');
+    buttons[0].click();
+    expect(component.increment).toHaveBeenCalled();
+  });
+
+  it('should call asyncIncrement', () => {
+    spyOn(component, 'asyncIncrement');
+    const buttons: NodeListOf<HTMLButtonElement> = fixture.debugElement.nativeElement.querySelectorAll('button');
+    buttons[2].click();
+    expect(component.asyncIncrement).toHaveBeenCalled();
+  });
 });
